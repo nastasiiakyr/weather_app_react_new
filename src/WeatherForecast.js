6366894580e0931789b14066d86858c436188164
Anastasiia <136587042+nastasiiakyr@users.noreply.github.com>
2024-01-25 14:39:37 +0200

    Added Forecast data for 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -5,8 +5,7 @@ import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
-  let [forecast, setForecast] = useState("")
-
+  let [forecast, setForecast] = useState(null);
 
   const fetchData = useCallback(async () => {
     const apiKeyWeather = "8cd9be374c7c96c39a9fe73f4bf2f055";
@@ -19,19 +18,42 @@ export default function WeatherForecast(props) {
   }, [props, fetchData]);
 
   function handleResponse(response) {
-    setForecast(response.data);
+    setForecast(response.data.daily);
   }
+  if (forecast) {
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
-  return (
-    <div className="WeatherForecast">
-      <div className="forecastDayCard">
-        <WeatherIcon icon="01d" condition="Clear" />
-        <div className="temperature">
-          <span className="max">12</span> / <span className="min">4</span>
-        </div>
-        <hr />
-        <h3 className="day_name">Saturday</h3>
+    return (
+      <div className="WeatherForecast">
+        {forecast.map(function (dailyForecast, index) {
+          if (index < 5) {
+            return (
+              <div className="forecastDayCard" key={index}>
+                <WeatherIcon
+                  icon={dailyForecast.weather[0].icon}
+                  condition={dailyForecast.weather[0].main}
+                />
+                <div className="temperature">
+                  <span className="max">{Math.round(dailyForecast.temp.max)}°</span> /{" "}
+                  <span className="min">{Math.round(dailyForecast.temp.min)}°</span>
+                </div>
+                <hr />
+                <h3 className="day_name">{days[(new Date(dailyForecast.dt * 1000)).getDay()]}</h3>
+              </div>
+            );
+          }
+
+          return null;
+        })}
       </div>
-    </div>
-  );
+    );
+  } else {return "";}
 }
